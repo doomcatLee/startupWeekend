8fe6d3cbe0928cf79591ad08e1c1a72d7b5da4f3
Alex Lee <doomcat9167@gmail.com>
2017-03-24 14:18:35 -0700

    add README.md

--- README.md
+++ README.md
@@ -1,17 +1,23 @@
-# _Squad_
+# StartUp Weekend
 
-#### _Squad, 03-23-2017_
+#### _StartUp Weekend, 03-24-2017_
 
-#### By _**Alex Lee and Josh Linton**_
+#### By _Alex Lee_
 
 ## Description
-_Example text for the description of the project_
+_Start up weekend takes a user input of teams and members inside it. It uses RESTful routing, so each member will have a unique id associated with unique team id in the URL_
 
 
 ## Specifications
 
 | Behavior                   | Input Example     | Output Example    |
 | -------------------------- | -----------------:| -----------------:|
+|Member object returns its id| none | int id|
+|Member object returns its name | none| String name|
+|Team object returns member object | none | member Object|
+|Team Object returns members id | none | int id|
+|Team Object adds member object | member object | none|
+|Team object returns a List Object of Members | none| List<Member>Object|
 
 
 
@@ -24,6 +30,6 @@ _Example text for the description of the project_
 
 ### License
 
-Copyright (c) 2017 **_Alex Lee and Josh Linton_**
+Copyright (c) 2017 **_Alex Lee_**
 
 This software is licensed under the MIT license.
--- src/test/java/TestJava.java
+++ src/test/java/TestJava.java
@@ -1,58 +1,41 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.util.List;
 
 public class TestJava{
 
-  // @Test
-  // public void HeroObjectInstantiation(){
-  //   Hero testH = new Hero("Igor", "fire", 100);
-  //   Squad testS = new Squad("Team1", "X", testH);
-  //   assertEquals(true, testS instanceof Squad);
-  // }
-  //
-  // @Test
-  // public void TestReturnHeroObject(){
-  //   Hero testH = new Hero("Igor", "fire", 100);
-  //   Squad testS = new Squad("Team1", "X", testH);
-  //   assertEquals(true, testS.getHero() instanceof Hero);
-  // }
-  //
-  // @Test
-  // public void clear_emptiesAllHeros_AND_SQUAD_From_List() {
-  //   Hero testH = new Hero("Igor", "fire", 100);
-  //   Squad testS = new Squad("Team1", "X", testH);
-  //   Squad.clear();
-  //   Hero.clear();
-  //   assertEquals(Hero.all().size(), 0);
-  //   assertEquals(Squad.all().size(), 0);
-  // }
-  //
-  // @Test
-  // public void find_returnsHeroWithSameId_secondHero() {
-  //   Hero.clear();
-  //   Hero firstH = new Hero("Igor", "fire", 100);
-  //   Hero secondH = new Hero("Dom", "water", 100);
-  //   assertEquals(Hero.find(secondH.getId()), secondH);
-  // }
-  //
-  // @Test
-  // public void find_returnsSquadWithSameId_secondSquad() {
-  //   Squad.clear();
-  //   Hero firstH = new Hero("Igor", "fire", 100);
-  //   Hero secondH = new Hero("Dom", "water", 100);
-  //   Squad firstS = new Squad("Igor", "fire", firstH);
-  //   Squad secondS = new Squad("Dom", "water", secondH);
-  //   assertEquals(Squad.find(secondS.getId()), secondS);
-  // }
-  //
-  // @Test
-  // public void add_Hero_adds_HeroList_true() {
-  //   Hero testH = new Hero("Dom", "water", 100);
-  //   Squad testS = new Squad("Igor", "fire", testH);
-  //   testS.addHero(testH);
-  //   assertTrue(testS.getHeros().contains(testH));
-  //   Hero.clear();
-  // }
+  //Variables for testing
+  Member member1 = new Member("Alex");
+  Member member2 = new Member("Jane");
+  Team team1 = new Team("Blossom");
+
+  @Test
+  public void object_instantiates_correctly(){
+    assertEquals(true, member1 instanceof Member);
+    assertEquals(true, team1 instanceof Team);
+  }
+
+  @Test
+  public void member_object_inside_of_team_works(){
+    team1.addMember(member1);
+    assertEquals(true, team1.getMembers() instanceof List<?>);
+  }
+
+  @Test
+  public void team_returns_the_right_id(){
+    team1.addMember(member1);
+    assertEquals(2, team1.getId());
+  }
+
+  @Test
+  public void team_returns_name(){
+    assertEquals("Blossom", team1.getName());
+  }
+
+  @Test
+  public void team_returns_list_of_all_instances(){
+    assertEquals(true, team1.all() instanceof List<?>);
+  }
 
 
 }
